198850e99f0bee1917029bc535bdca20d8197f83
Hem <112458846+SushilKc00@users.noreply.github.com>
2024-07-26 12:05:06 +0530

    Update pageBySlug.ts

--- apps/web/app/_lib/pageBySlug.ts
+++ apps/web/app/_lib/pageBySlug.ts
@@ -1,7 +1,7 @@
 import { createDirectus, readItems, rest } from '@directus/sdk';
 
-// const WEB_URL = process.env.NEXT_PUBLIC_URL as string;
-const directus = createDirectus('http://0.0.0.0:8055').with(rest());
+const WEB_URL = process.env.NEXT_PUBLIC_DIRECTUS_URL || 'https://directus-host.onrender.com';
+const directus = createDirectus(WEB_URL).with(rest());
 
 const pageBySlug = async (slug: string) => {
   try {
